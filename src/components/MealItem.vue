0bef0381375238c3e246b846f5527f33a995669c
Diana Cano <dcano31@stu.southtexascollege.edu>
2025-11-21 18:21:18 -0600

    Meal etails Working

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -9,15 +9,12 @@ const { meal } = defineProps({
 
 <template>
   <div class="bg-white shadow rounded-xl">
-    <router-link to="">
-      <img :src="meal.strMealThumb" class="rounded-t-xl h-48 object-cover w-full" />
+    <router-link :to="{name: 'MealDetails', params: {id: meal.idMeal}}">
+      <img :src="meal.strMealThumb" class="rounded-t-xl h-48 object-cover w-full"/>
     </router-link>
-        <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-        <p class="mb-4 line-clamp-3">{{ meal.strInstructions }}</p>
-    </div>
-    
   </div>
-
+  <div class="p-3">
+    <h3 class="font-bold">{{ meal.strMeal }}</h3>
+    <p class="mb-4 line-clamp-3">{{ meal.strInstructions }}</p>
+  </div>
 </template>
-
